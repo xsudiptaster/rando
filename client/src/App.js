e4d3e3f73cdf4fded60acf84221bb4bf07902c9f
skarmakar5 <skarmakar@salesforce.com>
2019-04-16 15:33:04 +0200

    s

--- client/src/App.js
+++ client/src/App.js
@@ -16,48 +16,6 @@ class App extends Component {
       .then(res => res.json())
       .then(passwords => this.setState({ passwords }));
   }
-  passwordrender(){
-    const { passwords } = this.state;
-    return (
-    <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.jhguyg</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-            hello
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
   loginpagescreen(){
     return(
     <div className="App">
@@ -104,8 +62,6 @@ class App extends Component {
     return (
       this.loginpagescreen()
       );
-  }
-
-  
+  }  
 }
 export default App;
