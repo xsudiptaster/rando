2c6a943821f752d21432d6a74e735d16212e4d7a
skarmakar5 <skarmakar@salesforce.com>
2019-04-18 00:07:44 +0200

    sd

--- client/src/App.js
+++ client/src/App.js
@@ -53,76 +53,7 @@ class App extends Component {
   loginpagerender() {
     const { passwords } = this.state;
     return (
-      <div><HeaderComp/></div>
-      
-      <div className="slds-grid slds-gutters">
-        <div className="slds-col">
-          <span />
-        </div>
-        <div class="slds-col">
-          <span>
-            <div className="App">
-              <table>
-                <tr>
-                  <td>
-                    <div className="slds-text-heading_large">
-                      Please Select the Org:
-                    </div>
-                  </td>
-                  <td>
-                    <select
-                      selectedvalue={this.state.loginurl}
-                      onChange={this.handleSelectChange.bind(this)}
-                    >
-                      <option value="https://test.salesforce.com">
-                        Sandbox
-                      </option>
-                      <option value="https://login.salesforce.com">
-                        Production
-                      </option>
-                    </select>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="slds-text-heading_large">Username :</div>
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      value={this.state.username}
-                      onChange={this.handleUsernameChange.bind(this)}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="slds-text-heading_large">Password :</div>
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      value={this.state.password}
-                      onChange={this.handlePasswordChange.bind(this)}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td />
-                  <td>
-                    <input
-                      type="button"
-                      value="Login"
-                      onClick={() => this.handlelogin()}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </span>
-        </div>
-        <div class="slds-col" />
-      </div>
+    <div><HeaderComp/></div>
     );
   }
   render() {
