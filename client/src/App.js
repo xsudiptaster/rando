628e91c4b5bd2c9197bd162bab365a46744e574f
skarmakar5 <skarmakar@salesforce.com>
2019-04-18 19:59:22 +0200

    sd

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
 import Headerdisplay from "./Headerdisplay.js"
-
+import LoginSection from "./LoginSection.js"
 class App extends Component {
   constructor(props) {
     super(props);
@@ -14,7 +14,9 @@ class App extends Component {
     
   }
   render() {
-    return (<div classclassName="slds-brand-band slds-brand-band_large slds-brand-band_group"></div>);
+    return (<div classclassName="slds-brand-band slds-brand-band_large slds-brand-band_group">
+    <LoginSection/>
+    </div>);
   }
 }
 export default App;
