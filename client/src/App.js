673a4b4099e4fc19f18b78bcb9f006e5e4c13781
skarmakar5 <skarmakar@salesforce.com>
2019-04-18 12:04:49 +0200

    sdf

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
-var headerdisplay = require( "./headerdisplay.js")
+import {headerdisplay} from "./headerdisplay.js"
 
 class HeaderComp extends Component {
   constructor(props)
