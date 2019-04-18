82ee22e25bde2e37e7e93b1282afcaf52021de72
skarmakar5 <skarmakar@salesforce.com>
2019-04-18 05:03:07 +0200

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import "./lightning-design/styles/salesforce-lightning-design-system.css";
 class HeaderComp extends Component {
   constructor(props)
   {
+    super(props);
     this.setState({session: this.props.session});
     console.log('The STate',this.state);
   }
