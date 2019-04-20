dd0bcc06014ef46148f89415909317120e3b1a0b
skarmakar5 <skarmakar@salesforce.com>
2019-04-20 15:19:41 +0200

    asd

--- client/src/ContentReviewerActions.jsx
+++ client/src/ContentReviewerActions.jsx
@@ -1,7 +1,7 @@
 var Reflux = require('reflux');
-var ContentReviewerActions = Reflux.createActions({
+var ContentReviewerActions = Reflux.createActions([
  // Child actions 'completed' and 'failed' are called by resolution of listenAndPromise
- "loginInSalesforce": {  },
- "stateupdates":{}, 
-});
+ "loginInSalesforce",
+ "stateupdates", 
+]);
 module.exports = ContentReviewerActions;
