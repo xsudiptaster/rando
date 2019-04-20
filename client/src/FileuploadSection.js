1006c3e50095f621be99503bf3ffc286949c33d4
skarmakar5 <skarmakar@salesforce.com>
2019-04-21 00:55:47 +0200

    sd

--- client/src/FileuploadSection.js
+++ client/src/FileuploadSection.js
@@ -7,22 +7,56 @@ var ContentReviewStore = require("./ContentReviewStore.jsx");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
 
 export default class FileuploadSection extends Reflux.Component {
-    constructor(props) {
-        mixins: [Reflux.connect(ContentReviewStore, "ContentReviewStore")];
-        super(props);
-        this.store=ContentReviewStore;
-      }
+  constructor(props) {
+    mixins: [Reflux.connect(ContentReviewStore, "ContentReviewStore")];
+    super(props);
+    this.store = ContentReviewStore;
+  }
 
-      render(){
-         return(
-            <div>
-            Hello
+  render() {
+    return (
+      <div className="slds-form-element">
+        <span
+          className="slds-form-element__label"
+          id="file-selector-primary-label"
+        >
+          Attachment
+        </span>
+        <div className="slds-form-element__control">
+          <div className="slds-file-selector slds-file-selector_files">
+            <div className="slds-file-selector__dropzone">
+              <input
+                className="slds-file-selector__input slds-assistive-text"
+                accept="image/png"
+                type="file"
+                id="file-upload-input-01"
+                aria-labelledby="file-selector-primary-label file-selector-secondary-label"
+              />
+              <label
+                className="slds-file-selector__body"
+                htmlFor="file-upload-input-01"
+                id="file-selector-secondary-label"
+              >
+                <span className="slds-file-selector__button slds-button slds-button_neutral">
+                  <svg
+                    className="slds-button__icon slds-button__icon_left"
+                    aria-hidden="true"
+                  >
+                    <use
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#upload"
+                    />
+                  </svg>
+                  Upload Files
+                </span>
+                <span className="slds-file-selector__text slds-medium-show">
+                  or Drop Files
+                </span>
+              </label>
             </div>
-         );   
-      }
-        
-
-      
-
-
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
