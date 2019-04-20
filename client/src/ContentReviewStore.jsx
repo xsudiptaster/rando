a363226d60d0bba37d8130c3b27388abe954f1b1
skarmakar5 <skarmakar@salesforce.com>
2019-04-20 15:30:22 +0200

    zxc

--- client/src/ContentReviewStore.jsx
+++ client/src/ContentReviewStore.jsx
@@ -1,34 +1,36 @@
-var Reflux = require('reflux');
-var ContentReviewerActions = require('./ContentReviewerActions.jsx');
+var Reflux = require("reflux");
+var ContentReviewerActions = require("./ContentReviewerActions.jsx");
 
 var ContentReviewerStore = Reflux.createStore({
- // Shorthand for listening to all ContentReviewerActions
- listenables: [ContentReviewerActions],
- firstdata :{} ,
- constructor()
-{
+  // Shorthand for listening to all ContentReviewerActions
+  listenables: [ContentReviewerActions],
+  firstdata: {},
+  constructor() {
     super();
-    this.listenTo(stateupdates, this.stateupdates);
-},
- // Load a review when the store is initialized
- init: function() {
-    this.firstdata.logindisplay={
-        visibility: "show"
+  },
+  // Load a review when the store is initialized
+  init: function() {
+    this.firstdata.logindisplay = {
+      visibility: "show"
     };
-    this.firstdata.uploadfiledisplay={
-        visibility: "hidden"
+    this.firstdata.uploadfiledisplay = {
+      visibility: "hidden"
     };
-    this.trigger(this.firstdata);
- },
- stateupdates : function(state){
-    this.firstdata=state;
-    console.log('The Store DAta',this.firstdata);
-    this.trigger(this.firstdata);
- },
- // Clear out the current review and any errors while we load the next review
- loginInSalesforce: function() {
-   
- },
+    this.fireUpdate();
+  },
+  stateupdates: function(state) {
+    this.firstdata = state;
+    console.log("The Store DAta", this.firstdata);
+    this.fireUpdate();
+  },
+
+  // Clear out the current review and any errors while we load the next review
+  loginInSalesforce: function() {},
+  fireUpdate:function ()
+  {
+    this.trigger('change',this.firstdata);
+  }
+
 });
 
 module.exports = ContentReviewerStore;
