8a8ce3bbf6bdbf00d48279bc5c1f239834cf9b77
skarmakar5 <skarmakar@salesforce.com>
2019-04-20 15:13:41 +0200

    as

--- client/src/ContentReviewerActions.jsx
+++ client/src/ContentReviewerActions.jsx
@@ -1,5 +1,4 @@
 var Reflux = require('reflux');
-
 var ContentReviewerActions = Reflux.createActions({
  // Child actions 'completed' and 'failed' are called by resolution of listenAndPromise
  "loginInSalesforce": {  },
