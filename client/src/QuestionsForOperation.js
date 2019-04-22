2f11db2768fb0404818e2a1dbb5f776be4b4c827
skarmakar5 <skarmakar@salesforce.com>
2019-04-22 15:30:55 +0200

    asd

--- client/src/QuestionsForOperation.js
+++ client/src/QuestionsForOperation.js
@@ -29,6 +29,57 @@ export default class FileuploadSection extends Reflux.Component {
     this.state.isRelated = event.target.value;
   }
   render() {
-    return (<div />);
+    return (
+      <div>
+        <div>
+          <article className="slds-card">
+            <div className="slds-card__header slds-grid">
+              <table className="slds-table">
+                <tr>
+                  <td>
+                    <div className="slds-media__body">
+                      <h2 className="slds-card__header-title">
+                        <input
+                          type="checkbox"
+                          className="slds-checkbox"
+                          onChange={this.isRealtedCheck.bind(this)}
+                        />
+                        &nbsp;&nbsp;
+                        <a
+                          href="javascript:void(0);"
+                          className="slds-card__header-link slds-truncate"
+                          title="Accounts"
+                        >
+                          <span>Are the Sheets Related ?</span>
+                        </a>
+                      </h2>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="slds-media__body">
+                      <h2 className="slds-card__header-title">
+                        <a
+                          href="javascript:void(0);"
+                          className="slds-card__header-link slds-truncate"
+                          title="Accounts"
+                        >
+                          <span>Sheets to Insert ?</span>
+                        </a>
+                      </h2>
+                    </div>
+                    <div>Sheet A</div>
+                    <div>Sheet</div>
+                  </td>
+                </tr>
+              </table>
+              <br />
+              <br />
+            </div>
+          </article>
+        </div>
+      </div>
+    );
   }
 }
