dba5d389e0a695f01e372f82258c644331c35ff6
skarmakar5 <skarmakar@salesforce.com>
2019-04-24 00:32:03 +0200

    asd

--- client/src/QuestionsForOperation.js
+++ client/src/QuestionsForOperation.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-import axios from "axios";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
 import readXlsxFile from "read-excel-file";
+
 var Reflux = require("reflux");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
@@ -15,82 +15,83 @@ export default class FileuploadSection extends Reflux.Component {
       isRelated: false
     });
   }
+
   readthefile(event) {
     this.setState({
       fileBlob: event.target.files[0]
     });
-    readXlsxFile(event.target.files[0], { getSheets: true }).then(data => {
+      readXlsxFile(event.target.files[0], {getSheets: true}).then(data => {
       this.setState({
         sheetNames: data
       });
     });
   }
+
   isRealtedCheck(event) {
     this.state.isRelated = event.target.value;
   }
-  render() {
+
+    render() {
     var rowsdv = new Array();
     if (this.state && this.state.sheetNames != undefined) {
-      console.log('this.state',this.state);
-      for (var i = 0; i < this.state.sheetNames.length; i++) {
-        rowsdv.push(this.state.sheetNames[i]);
-      }
+        console.log('this.state', this.state);
+        rowsdv = this.state.sheetNames;
     }
-    console.log('Rows',rowsdv);
+        console.log('Rows', rowsdv);
     return (
-      <div>
         <div>
-          <article className="slds-card">
-            <div className="slds-card__header slds-grid">
-              <table className="slds-table">
-                <tr>
-                  <td>
-                    <div className="slds-media__body">
-                      <h2 className="slds-card__header-title">
-                        <input
-                          type="checkbox"
-                          className="slds-checkbox"
-                          onChange={this.isRealtedCheck.bind(this)}
-                        />
-                        &nbsp;&nbsp;
-                        <a
-                          href="javascript:void(0);"
-                          className="slds-card__header-link slds-truncate"
-                          title="Accounts"
-                        >
-                          <span>Are the Sheets Related ?</span>
-                        </a>
-                      </h2>
+            <div>
+                <article className="slds-card">
+                    <div className="slds-card__header slds-grid">
+                        <table className="slds-table">
+                            <tr>
+                                <td>
+                                    <div className="slds-media__body">
+                                        <h2 className="slds-card__header-title">
+                                            <input
+                                                type="checkbox"
+                                                className="slds-checkbox"
+                                                onChange={this.isRealtedCheck.bind(this)}
+                                            />
+                                            &nbsp;&nbsp;
+                                            <a
+                                                href="javascript:void(0);"
+                                                className="slds-card__header-link slds-truncate"
+                                                title="Accounts"
+                                            >
+                                                <span>Are the Sheets Related ?</span>
+                                            </a>
+                                        </h2>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className="slds-media__body">
+                                        <h2 className="slds-card__header-title">
+                                            <a
+                                                href="javascript:void(0);"
+                                                className="slds-card__header-link slds-truncate"
+                                                title="Accounts"
+                                            >
+                                                <span>Sheets to Insert ?</span>
+                                            </a>
+                                        </h2>
+                                    </div>
+                                    <div>Sheet A</div>
+                                    <div>Sheet B</div>
+                                    {rowsdv.map(function (name, index) {
+                                        return <div key={index}>{name}</div>;
+                                    })}
+                                </td>
+                            </tr>
+                        </table>
+                        <br/>
+                        <br/>
                     </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="slds-media__body">
-                      <h2 className="slds-card__header-title">
-                        <a
-                          href="javascript:void(0);"
-                          className="slds-card__header-link slds-truncate"
-                          title="Accounts"
-                        >
-                          <span>Sheets to Insert ?</span>
-                        </a>
-                      </h2>
-                    </div>
-                    <div>Sheet A</div>
-                    <div>Sheet B</div>
-                    {rowsdv.map(function(name, index){
-                    return <div key={ index }>{name}</div>;
-                  })}
-                  </td>
-                </tr>
-              </table>
-              <br />
-              <br />
+                </article>
             </div>
-          </article>
         </div>
-      </div>
     );
   }
 }
