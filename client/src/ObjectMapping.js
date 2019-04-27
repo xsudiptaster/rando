f5bab05d2d12d60a69daa7af44aa2038f47257ca
skarmakar5 <skarmakar@salesforce.com>
2019-04-27 23:21:53 +0200

    asd

--- client/src/ObjectMapping.js
+++ client/src/ObjectMapping.js
@@ -22,6 +22,7 @@ export default class ObjectMapping extends Reflux.Component {
                     for (var j = 0; j < data[0].length; j++) {
                         optns.push(data[0][i]);
                     }
+                    console.log('The State Value is here', this.state);
                     this.state.objectMapping[i].sheetHeaders = optns;
                     console.log('The Headers', this.state.objectMapping[i].sheetHeaders);
                 });
