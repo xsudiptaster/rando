f9f645d4a481af7f2e4e77759336fb85c2f422db
skarmakar5 <skarmakar@salesforce.com>
2019-04-29 12:11:24 +0200

    asd

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/api/objectList", function(req, res) {
       return console.error(err);
     }
     var listObjects=[]
-    var listallObjects=Json.parse(jsobj.sobjects);
+    var listallObjects=JSON.parse(jsobj.sobjects);
     for (var i=0;i<listallObjects.length;i++)
     {
       if(listallObjects[i].createable){
