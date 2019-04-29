a226ad068da2a0a32fc0f6f12e480f9923cbc896
skarmakar5 <skarmakar@salesforce.com>
2019-04-30 00:34:15 +0200

    asd

--- client/src/ObjectMapping.js
+++ client/src/ObjectMapping.js
@@ -13,7 +13,6 @@ export default class ObjectMapping extends Reflux.Component {
   constructor(props) {
     super(props);
     this.store = ContentReviewStore;
-    this.state.ObjectDesb = {};
   }
 
   columnnOptions() {
