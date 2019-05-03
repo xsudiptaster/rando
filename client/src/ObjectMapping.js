9076313aeb3bf68c69b2bac7598eb881a4159e80
skarmakar5 <skarmakar@salesforce.com>
2019-05-03 22:51:13 +0200

    asd

--- client/src/ObjectMapping.js
+++ client/src/ObjectMapping.js
@@ -114,7 +114,7 @@ export default class ObjectMapping extends Reflux.Component {
                             <td>
                                 <input className="slds-input" list="mylist" width="100px"
                                        onChange={this.onchangeObjectSelection.bind(this, value)}/>
-                                <datalist className="slds-list_horizontal" id="mylist" style="display:hidden"
+                                <datalist className="slds-list_horizontal" id="mylist" style="{display:hidden}"
                                 >
 
                                     {this.state.objectList.map(val => (
