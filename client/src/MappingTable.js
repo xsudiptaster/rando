8e446f5f7df1eff9662ca94befe33343ec0ca6b8
skarmakar5 <skarmakar@salesforce.com>
2019-05-04 22:38:13 +0200

    asd

--- client/src/MappingTable.js
+++ client/src/MappingTable.js
@@ -1,8 +1,8 @@
 import React              from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
-import SLDSAccordion      from '@salesforce';
-import SLDSAccordionpanel from '@salesforce';
+import SLDSAccordion      from '@salesforce/design-system-react';
+import SLDSAccordionpanel from '@salesforce/design-system-react';
 
 var Reflux = require("reflux");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
