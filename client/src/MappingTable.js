b11a1939d7be55a117a0f2e46f45a4c80989c1fe
skarmakar5 <skarmakar@salesforce.com>
2019-05-04 22:19:11 +0200

    asd

--- client/src/MappingTable.js
+++ client/src/MappingTable.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React              from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
+import "./lightning-design/styles/";
+import SLDSAccordion      from '@salesforce/design-system-react/components/button';
+import SLDSAccordionpanel from '@salesforce/design-system-react/components/button';
 
 var Reflux = require("reflux");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
@@ -15,170 +18,11 @@ export default class MappingTable extends Reflux.Component {
     render() {
         return (
             <div>
-                <ul className="slds-accordion">
-                    <li className="slds-accordion__list-item">
-                        <section className="slds-accordion__section slds-is-open">
-                            <div className="slds-accordion__summary">
-                                <h3 className="slds-accordion__summary-heading">
-                                    <button aria-controls="accordion-details-01" aria-expanded="true"
-                                            className="slds-button slds-button_reset slds-accordion__summary-action">
-                                        <svg
-                                            className="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left"
-                                            aria-hidden="true">
-                                            <use xlinkHref="http://www.w3.org/1999/xlink"
-                                                 xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#switch"/>
-                                        </svg>
-                                        <span className="slds-truncate"
-                                              title="Accordion summary">Accordion summary</span>
-                                    </button>
-                                </h3>
-                                <div className="slds-dropdown-trigger slds-dropdown-trigger_click">
-                                    <button
-                                        className="slds-button slds-button_icon slds-button_icon-border-filled slds-button_icon-x-small"
-                                        aria-haspopup="true" title="Show More">
-                                        <svg className="slds-button__icon" aria-hidden="true">
-                                            <use xlinkHref="http://www.w3.org/1999/xlink"
-                                                 xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#down"/>
-                                        </svg>
-                                        <span className="slds-assistive-text">Show More</span>
-                                    </button>
-                                    <div className="slds-dropdown slds-dropdown_actions slds-dropdown_right">
-                                        <ul className="slds-dropdown__list" role="menu">
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="0">
-                                                    <span className="slds-truncate" title="Action One">Action One</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate" title="Action Two">Action Two</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate"
-                                                          title="Action Three">Action Three</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div aria-hidden="false" className="slds-accordion__content"
-                                 id="accordion-details-01">Accordion
-                                details - A
-                            </div>
-                        </section>
-                    </li>
-                    <li className="slds-accordion__list-item">
-                        <section className="slds-accordion__section">
-                            <div className="slds-accordion__summary">
-                                <h3 className="slds-accordion__summary-heading">
-                                    <button aria-controls="accordion-details-02" aria-expanded="false"
-                                            className="slds-button slds-button_reset slds-accordion__summary-action">
-                                        <svg
-                                            className="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left"
-                                            aria-hidden="true">
-                                            <use xlinkHref="http://www.w3.org/1999/xlink"
-                                                 xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#switch"/>
-                                        </svg>
-                                        <span className="slds-truncate"
-                                              title="Accordion summary">Accordion summary</span>
-                                    </button>
-                                </h3>
-                                <div className="slds-dropdown-trigger slds-dropdown-trigger_click">
-                                    <button
-                                        className="slds-button slds-button_icon slds-button_icon-border-filled slds-button_icon-x-small"
-                                        aria-haspopup="true" title="Show More">
-                                        <svg className="slds-button__icon" aria-hidden="true">
-                                            <use xlinkHref="http://www.w3.org/1999/xlink"
-                                                 xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#down"/>
-                                        </svg>
-                                        <span className="slds-assistive-text">Show More</span>
-                                    </button>
-                                    <div className="slds-dropdown slds-dropdown_actions slds-dropdown_right">
-                                        <ul className="slds-dropdown__list" role="menu">
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="0">
-                                                    <span className="slds-truncate" title="Action One">Action One</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate" title="Action Two">Action Two</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate"
-                                                          title="Action Three">Action Three</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div aria-hidden="true" className="slds-accordion__content"
-                                 id="accordion-details-02">Accordion
-                                details - B
-                            </div>
-                        </section>
-                    </li>
-                    <li className="slds-accordion__list-item">
-                        <section className="slds-accordion__section">
-                            <div className="slds-accordion__summary">
-                                <h3 className="slds-accordion__summary-heading">
-                                    <button aria-controls="accordion-details-03" aria-expanded="false"
-                                            className="slds-button slds-button_reset slds-accordion__summary-action">
-                                        <svg
-                                            className="slds-accordion__summary-action-icon slds-button__icon slds-button__icon_left"
-                                            aria-hidden="true">
-                                            <use xmlnsxlink="http://www.w3.org/1999/xlink"
-                                                 xlinkhref="/assets/icons/utility-sprite/svg/symbols.svg#switch"/>
-                                        </svg>
-                                        <span className="slds-truncate"
-                                              title="Accordion summary">Accordion summary</span>
-                                    </button>
-                                </h3>
-                                <div className="slds-dropdown-trigger slds-dropdown-trigger_click">
-                                    <button
-                                        className="slds-button slds-button_icon slds-button_icon-border-filled slds-button_icon-x-small"
-                                        aria-haspopup="true" title="Show More">
-                                        <svg className="slds-button__icon" aria-hidden="true">
-                                            <use xmlnsxlink="http://www.w3.org/1999/xlink"
-                                                 xlinkhref="/assets/icons/utility-sprite/svg/symbols.svg#down"/>
-                                        </svg>
-                                        <span className="slds-assistive-text">Show More</span>
-                                    </button>
-                                    <div className="slds-dropdown slds-dropdown_actions slds-dropdown_right">
-                                        <ul className="slds-dropdown__list" role="menu">
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="0">
-                                                    <span className="slds-truncate" title="Action One">Action One</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate" title="Action Two">Action Two</span>
-                                                </a>
-                                            </li>
-                                            <li className="slds-dropdown__item" role="presentation">
-                                                <a href="javascript:void(0);" role="menuitem" tabIndex="-1">
-                                                    <span className="slds-truncate"
-                                                          title="Action Three">Action Three</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div aria-hidden="true" className="slds-accordion__content"
-                                 id="accordion-details-03">Accordion
-                                details - C
-                            </div>
-                        </section>
-                    </li>
-                </ul>
+                <SLDSAccordion>
+                    <SLDSAccordionpanel>
+                        Hello
+                    </SLDSAccordionpanel>
+                </SLDSAccordion>
             </div> );
     }
 }
