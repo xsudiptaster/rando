30dea427cce713d32f64ae0602cbbc9bac9c8e09
skarmakar5 <skarmakar@salesforce.com>
2019-05-04 23:27:48 +0200

    asd

--- client/src/MappingTable.js
+++ client/src/MappingTable.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React                 from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
+import ExpansionPanel        from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography            from '@material-ui/core/Typography';
+import ExpandMoreIcon        from '@material-ui/icons/ExpandMore';
 
 var Reflux = require("reflux");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
+
+const styles = theme => ( {
+    root   : {
+        width: '100%',
+    },
+    heading: {
+        fontSize  : theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+} );
 
 export default class MappingTable extends Reflux.Component {
     constructor(props) {
@@ -17,7 +32,19 @@ export default class MappingTable extends Reflux.Component {
 
     render() {
         return (
-            <div></div>
+            <div>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography>Expansion Panel 1</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
         );
     }
 }
