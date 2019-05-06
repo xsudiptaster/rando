50abc93048c5c9321bf32fae3fa24b9592c7a21d
skarmakar5 <skarmakar@salesforce.com>
2019-05-06 14:50:11 +0200

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ class App extends Reflux.Component {
             },
             errorModal          : {height: '14rem', display: 'none'},
             errorMessage        : "",
-
+            showProgress        : {display: "block"}
         } );
         ContentReviewerActions.stateupdates(this.state);
     }
@@ -115,7 +115,7 @@ class App extends Reflux.Component {
                                 <div className="slds-backdrop slds-backdrop_open"/>
                             </div>
                         </div>
-                        <div>
+                        <div style={this.state.showProgress}>
                             <CircularProgress style={{
                                 position       : "absolute", top: 0, left: 0, width: "100%", height: "100%",
                                 zIndex         : "10",
