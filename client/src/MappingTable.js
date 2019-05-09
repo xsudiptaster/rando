874cad30180b3ba69237fb7044067be000df83e5
skarmakar5 <skarmakar@salesforce.com>
2019-05-09 19:48:27 +0200

    asd

--- client/src/MappingTable.js
+++ client/src/MappingTable.js
@@ -55,10 +55,13 @@ export default class MappingTable extends Reflux.Component {
         var sheetNames = Object.keys(this.state.objectMapping);
         for (var i = 0; i < sheetNames; i++) {
             var objName = this.state.objectMapping[sheetNames[i]].ObjectName;
+            console.log('The Object Name', objName);
             for (var j = 0; j < this.state.objectMapping[sheetNames[i]].sheetHeaders.length; j++) {
                 var sheetHeader = this.state.objectMapping[sheetNames[i]].sheetHeaders[j];
+                console.log('SheetHeader', sheetHeader);
                 for (var k = 0; k < this.state.ObjectDesb[objName].fields.length; k++) {
                     var field = this.state.ObjectDesb[objName].fields[k];
+                    console.log('Fields received', field);
                     if (sheetHeader.indexOf(field.label) > -1 || sheetHeader.indexOf(field.name) > -1) {
                         console.log('The Matching', sheetHeader);
                         console.log('The fieldname', field.name)
@@ -99,7 +102,6 @@ export default class MappingTable extends Reflux.Component {
                     }
                 }
             }
-            console.log('The ExternalId', extrDesbs);
         }
         return (
 
