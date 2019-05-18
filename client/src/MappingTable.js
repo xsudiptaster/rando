c063a49cdd104aac8444efb3c0888d2d50665984
skarmakar5 <skarmakar@salesforce.com>
2019-05-18 20:08:38 +0200

    asd

--- client/src/MappingTable.js
+++ client/src/MappingTable.js
@@ -59,6 +59,7 @@ export default class MappingTable extends Reflux.Component {
         this.state.ErrorLog = {};
         this.state.objectmappingdisplay = {display: "none"};
         this.state.finalupsertresult = {display: "block"};
+        ContentReviewerActions.stateupdates(this.state);
         console.log('The Upsert called', this.state);
         if (this.state.isParallel) {
             for (var i = 0; i < this.state.sheetsToInsert.length; i++) {
