9af69af9e8559d87578fc5ba1880c97aa2b88c12
skarmakar5 <skarmakar@salesforce.com>
2019-05-19 00:42:36 +0200

    asd

--- client/src/StatusResult.js
+++ client/src/StatusResult.js
@@ -25,6 +25,7 @@ export default class StatusResult extends Reflux.Component {
             cal = ( this.state.objectMapping[Sheetname].sheetUpsertCalled /
                 this.state.objectMapping[Sheetname].sheetDataJsonList.length );
         }
+        console.log('The CAl', cal);
         return cal;
     }
 
