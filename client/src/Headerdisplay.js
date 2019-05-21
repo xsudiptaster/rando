3d0d024e8a5c340e69c8454ca825480ef4bb08bf
skarmakar5 <skarmakar@salesforce.com>
2019-05-21 23:53:41 +0200

    sd

--- client/src/Headerdisplay.js
+++ client/src/Headerdisplay.js
@@ -1,7 +1,11 @@
-import React                                                from "react";
+import React from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
-import { AppLauncher, AppLauncherSection, AppLauncherTile } from '@salesforce/design-system-react';
+import {
+  AppLauncher,
+  AppLauncherSection,
+  AppLauncherTile
+} from "@salesforce/design-system-react";
 
 var Reflux = require("reflux");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
@@ -9,45 +13,45 @@ var ContentReviewStore = require("./ContentReviewStore.jsx");
 export default class Headerdisplay extends Reflux.Component {
   constructor(props) {
     super(props);
-      this.store = ContentReviewStore;
+    this.store = ContentReviewStore;
   }
   componentDidMount() {}
   render() {
-      var CurrentApp = this.state != undefined ? this.state.currentApp : '';
+    var CurrentApp = this.state != undefined ? this.state.currentApp : "";
     return (
       <article className="slds-card">
         <div className="slds-card__header slds-grid">
           <header className="slds-media slds-media_center slds-has-flexi-truncate">
             <div className="slds-media__figure">
               <span
-                  className="slds-icon_container slds-icon-standard-account"
-                  title="account">
-                  <AppLauncher>
-    <AppLauncherSection title="App Selection">
-								<AppLauncherTile
-                                    title="Upsert Records"
-                                    iconText="UR"
-                                    description="Upsert Objects serially or parallel and use external Ids to relate them"
-                                />
-								<AppLauncherTile
-                                    title="Sample Data Creator"
-                                    description="This tool lets you create sample data for an Object and its related objects"
-                                    iconText="SR"
-                                />
-							</AppLauncherSection>
-</AppLauncher>
-
+                className="slds-icon_container slds-icon-standard-account"
+                title="account"
+              >
+                <AppLauncher>
+                  <AppLauncherSection title="App Selection">
+                    <AppLauncherTile
+                      title="Upsert Records"
+                      iconText="UR"
+                      description="Upsert Objects serially or parallel and use external Ids to relate them"
+                    />
+                    <AppLauncherTile
+                      title="Sample Data Creator"
+                      description="This tool lets you create sample data for an Object and its related objects"
+                      iconText="SR"
+                    />
+                  </AppLauncherSection>
+                </AppLauncher>
                 <span className="slds-assistive-text">First Heroku App</span>
               </span>
             </div>
             <div className="slds-media__body">
               <h2 className="slds-card__header-title">
                 <a
-                    href="javascript:void(0);"
-                    className="slds-card__header-link slds-truncate"
-                    title="First Heroku App">
-                    <span>First Heroku App:{CurrentApp} </span>
-
+                  href="javascript:void(0);"
+                  className="slds-card__header-link slds-truncate"
+                  title="First Heroku App"
+                >
+                  <span>First Heroku App:{CurrentApp} </span>
                 </a>
               </h2>
             </div>
