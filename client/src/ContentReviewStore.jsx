7b33c7480f70b93f18b22dd4b2f61dd8680c073a
skarmakar5 <skarmakar@salesforce.com>
2019-05-22 09:05:38 +0200

    asd

--- client/src/ContentReviewStore.jsx
+++ client/src/ContentReviewStore.jsx
@@ -1,67 +1,57 @@
 var Reflux = require("reflux");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
-var axios = require('axios');
+var axios = require("axios");
 
 var ContentReviewerStore = Reflux.createStore({
-// Shorthand for listening to all ContentReviewerActions
-                                                  listenables: [ContentReviewerActions],
-                                                  constructor() {
+  // Shorthand for listening to all ContentReviewerActions
+  listenables: [ContentReviewerActions],
+  constructor() {},
+  // Load a review when the store is initialized
+  init: function() {},
+  stateupdates: function(state) {
+    if (!this.firstdata) {
+      this.firstdata = {};
+    }
+    this.firstdata = state;
+    this.fireUpdate();
+  },
+  setvalparam: function(param, val) {
+    if (!this.firstdata) {
+      this.firstdata = {};
+    }
+    this.firstdata[param] = {};
+    this.firstdata[param] = val;
+    this.fireUpdate();
+  },
 
-                                                  },
-// Load a review when the store is initialized
-                                                  init          : function () {
-                                                  },
-                                                  stateupdates  : function (state) {
-                                                      if (!this.firstdata) {
-                                                          this.firstdata = {};
-                                                      }
-                                                      this.firstdata = state;
-                                                      this.fireUpdate();
-                                                  },
-                                                  setvalparam   : function (param, val) {
-                                                      if (!this.firstdata) {
-                                                          this.firstdata = {};
-                                                      }
-                                                      this.firstdata[param] = {};
-                                                      this.firstdata[param] = val;
-                                                      this.fireUpdate();
-                                                  },
-
-// Clear out the current review and any errors while we load the next review
-                                                  describeObject: function (objName, state) {
-                                                      if (!this.firstdata) {
-                                                          this.firstdata = {};
-                                                      }
-                                                      this.firstdata = state;
-                                                      if (this.firstdata != undefined && this.firstdata.sessiontok !=
-                                                          undefined) {
-                                                          axios.post("/api/objectDescribe", {
-                                                              sessiontok: this.firstdata.sessiontok,
-                                                              oUrl      : this.firstdata.instanceUrl,
-                                                              objName   : objName
-                                                          })
-                                                               .then(response => {
-                                                                   if (this.firstdata.ObjectDesb == undefined) {
-                                                                    this.firstdata.ObjectDesb = {};
-                                                                    }
-                                                                    this.firstdata.ObjectDesb[response.data.name] =
-                                                                    response.data;
-                                                                    console.log('The Return Val', response.data);
-                                                                   this.fireUpdate();
-
-                                                               })
-                                                               .catch(error => {
-                                                                   console.log(error);
-
-                                                               });
-
-                                                      }
-
-                                                  },
-                                                  fireUpdate    : function () {
-                                                      this.trigger(this.firstdata);
-                                                  },
-
-                                              });
+  // Clear out the current review and any errors while we load the next review
+  describeObject: function(objName, state) {
+    if (!this.firstdata) {
+      this.firstdata = {};
+    }
+    this.firstdata = state;
+    if (this.firstdata != undefined && this.firstdata.sessiontok != undefined) {
+      axios
+        .post("/api/objectDescribe", {
+          sessiontok: this.firstdata.sessiontok,
+          oUrl: this.firstdata.instanceUrl,
+          objName: objName
+        })
+        .then(response => {
+          if (this.firstdata.ObjectDesb == undefined) {
+            this.firstdata.ObjectDesb = {};
+          }
+          this.firstdata.ObjectDesb[response.data.name] = response.data;
+          this.fireUpdate();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  fireUpdate: function() {
+    this.trigger(this.firstdata);
+  }
+});
 
 module.exports = ContentReviewerStore;
--- client/src/ContentReviewerActions.jsx
+++ client/src/ContentReviewerActions.jsx
@@ -1,8 +1,8 @@
-var Reflux = require('reflux');
+var Reflux = require("reflux");
 var ContentReviewerActions = Reflux.createActions([
- // Child actions 'completed' and 'failed' are called by resolution of listenAndPromise
-                                                      "describeObject",
- "stateupdates", 
- "setvalparam",
+  // Child actions 'completed' and 'failed' are called by resolution of listenAndPromise
+  "describeObject",
+  "stateupdates",
+  "setvalparam"
 ]);
 module.exports = ContentReviewerActions;
