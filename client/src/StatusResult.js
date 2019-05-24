e3ee9410b08e5a75b1028280de5c662d30b27a77
skarmakar5 <skarmakar@salesforce.com>
2019-05-24 23:45:47 +0200

    sdd

--- client/src/StatusResult.js
+++ client/src/StatusResult.js
@@ -7,6 +7,7 @@ import Typography from "@material-ui/core/Typography";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import XLSX from "xlsx";
+import $ from "jquery";
 
 var Reflux = require("reflux");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
@@ -21,7 +22,7 @@ export default class StatusResult extends Reflux.Component {
 		var wb = { SheetNames: [], Sheets: {} };
 		for (var i = 0; i < this.state.sheetsToInsert.length; i++) {
 			var sheetName = this.state.sheetsToInsert[i];
-			var tbl = document.getElementById("log-" + sheetName);
+			var tbl = $(this.refs["log-" + sheetName]);
 			var ws = XLSX.utils.table_to_sheet(tbl);
 			wb.SheetNames.push(sheetName);
 			wb.Sheets[sheetName] = ws;
@@ -44,56 +45,61 @@ export default class StatusResult extends Reflux.Component {
 		return (
 			<div>
 				<div style={{ float: "right" }}>
-					<input type="button" value="Download Log" onClick={this.handleDownload.bind(this)}>
+					<input type="button" onClick={this.handleDownload.bind(this)}>
 						Download Log
 					</input>
 				</div>
-				{SheetNames.map(value => (
-					<ExpansionPanel>
-						<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-							<table>
-								<tr>
-									<td>
-										<Typography>
-											<label className="slds-text-heading_medium">{value}</label>
-										</Typography>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<progress
-											className="slds-progress-bar"
-											value={
-												ObjMapping[value] != undefined ? ObjMapping[value].sheetUpsertCalled : 0
-											}
-											max={
-												ObjMapping[value] != undefined
-													? ObjMapping[value].sheetDataJsonList.length
-													: 0
-											}
-										/>
-									</td>
-								</tr>
-							</table>
-						</ExpansionPanelSummary>
-						<ExpansionPanelDetails>
-							<div style={{ maxHeight: "400px", overflow: "scroll" }}>
-								<table className="resultTable" id={"log-" + value}>
-									{(ErrorLog[value] != undefined ? ErrorLog[value] : []).map((val, key) => (
-										<tr>
-											<td className="slds-wrap ">
-												<label style={{ fontWeight: 700 }}>{key}</label>
-											</td>
-											<td className="slds-wrap" style={{ borderBottom: "3px solid black" }}>
-												{val}
-											</td>
-										</tr>
-									))}
+
+				<div>
+					{SheetNames.map(value => (
+						<ExpansionPanel>
+							<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+								<table>
+									<tr>
+										<td>
+											<Typography>
+												<label className="slds-text-heading_medium">{value}</label>
+											</Typography>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<progress
+												className="slds-progress-bar"
+												value={
+													ObjMapping[value] != undefined
+														? ObjMapping[value].sheetUpsertCalled
+														: 0
+												}
+												max={
+													ObjMapping[value] != undefined
+														? ObjMapping[value].sheetDataJsonList.length
+														: 0
+												}
+											/>
+										</td>
+									</tr>
 								</table>
-							</div>
-						</ExpansionPanelDetails>
-					</ExpansionPanel>
-				))}
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<div style={{ maxHeight: "400px", overflow: "scroll" }}>
+									<table className="resultTable" ref={"log-" + value}>
+										{(ErrorLog[value] != undefined ? ErrorLog[value] : []).map((val, key) => (
+											<tr>
+												<td className="slds-wrap ">
+													<label style={{ fontWeight: 700 }}>{key}</label>
+												</td>
+												<td className="slds-wrap" style={{ borderBottom: "3px solid black" }}>
+													{val}
+												</td>
+											</tr>
+										))}
+									</table>
+								</div>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+					))}
+				</div>
 			</div>
 		);
 	}
