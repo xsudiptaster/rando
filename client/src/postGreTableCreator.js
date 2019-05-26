a74f3a0f8c944a3412a58eb3eae7c0f81beca65d
skarmakar5 <skarmakar@salesforce.com>
2019-05-26 14:05:16 +0200

    sd

---
+++ client/src/postGreTableCreator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import "./lightning-design/styles/salesforce-lightning-design-system.css";
+import SimpleCrypto from "simple-crypto-js";
+import $ from "jquery";
+import Cookies from 'universal-cookie';
+var Reflux = require("reflux");
+var ContentReviewStore = require("./ContentReviewStore.jsx");
+var ContentReviewerActions = require("./ContentReviewerActions.jsx");
+
+
+export default class postGreTableCreator extends Reflux.Component {
+
+    
+}
