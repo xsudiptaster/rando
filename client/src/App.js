e602ec780e933c3d6cf414527ed2f4423bc6d742
skarmakar5 <skarmakar@salesforce.com>
2019-05-29 23:13:42 +0200

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
 import UpsertApp from "./UpsertApp.js";
-import FileuploadSection from "./FileuploadSection.js";
-import QuestionsForOperation from "./QuestionsForOperation";
-import ObjectMapping from "./ObjectMapping";
-import MappingTable from "./MappingTable";
-import Headerdisplay from "./Headerdisplay";
-import StatusResult from "./StatusResult";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Modal from "@material-ui/core/Modal";
-import { BrandBand } from "@salesforce/design-system-react";
 
 var Reflux = require("reflux");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {browserHistory, Router} from 'react-router';
+import routes from './routes';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById('root'));
 registerServiceWorker();
