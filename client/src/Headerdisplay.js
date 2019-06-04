a25658a6b70dacd09434e97e2e11907ffd5248a7
skarmakar5 <skarmakar@salesforce.com>
2019-06-04 23:37:55 +0200

    asd

--- client/src/Headerdisplay.js
+++ client/src/Headerdisplay.js
@@ -7,63 +7,65 @@ var Reflux = require("reflux");
 var ContentReviewerActions = require("./ContentReviewerActions.jsx");
 var ContentReviewStore = require("./ContentReviewStore.jsx");
 export default class Headerdisplay extends Reflux.Component {
-	constructor(props) {
-		super(props);
-		this.store = ContentReviewStore;
-	}
-	componentDidMount() {}
+    constructor(props) {
+        super(props);
+        this.store = ContentReviewStore;
+    }
 
-	render() {
-		var CurrentApp = this.state != undefined ? this.state.currentApp : "";
-		return (
-			<article className="slds-card">
-				<div className="slds-card__header slds-grid">
-					<header className="slds-media slds-media_center slds-has-flexi-truncate">
-						<div className="slds-media__figure">
+    componentDidMount() {
+    }
+
+    render() {
+        var CurrentApp = this.state != undefined ? this.state.currentApp : "";
+        return (
+            <article className="slds-card">
+                <div className="slds-card__header slds-grid">
+                    <header className="slds-media slds-media_center slds-has-flexi-truncate">
+                        <div className="slds-media__figure">
 							<span title="account">
 								<AppLauncher>
 									<AppLauncherSection title="App Selection">
 										<AppLauncherTile
-											title="Upsert Records"
-											iconText="UR"
-											description="Upsert Objects serially or parallel and use external Ids to relate them"
-											onClick={() =>
-												function(event) {
-													this.state.currentApp = "Upsert";
-													ContentReviewerActions.stateupdates(this.state);
-												}
-											}
-										/>
+                                            title="Upsert Records"
+                                            iconText="UR"
+                                            description="Upsert Objects serially or parallel and use external Ids to relate them"
+                                            onClick={() =>
+                                                function (event) {
+                                                    this.state.currentApp = "Upsert";
+                                                    ContentReviewerActions.stateupdates(this.state);
+                                                }
+                                            }
+                                        />
 										<AppLauncherTile
-											title="Sample Data Creator"
-											description="This tool lets you create sample data for an Object and its related objects"
-											iconText="SR"
-										/>
+                                            title="Sample Data Creator"
+                                            description="This tool lets you create sample data for an Object and its related objects"
+                                            iconText="SR"
+                                        />
 										<AppLauncherTile
-											title="PostgreSQL Editor"
-											description="This tool lets you maintain a postGreSQL table"
-											iconText="PE"
-										/>
+                                            title="PostgreSQL Editor"
+                                            description="This tool lets you maintain a postGreSQL table"
+                                            iconText="PE"
+                                        />
 									</AppLauncherSection>
 								</AppLauncher>
 							</span>
-						</div>
+                        </div>
                         <div className="slds-media__body">
-							<h2 className="slds-card__header-title">
-								<a
-									href="javascript:void(0);"
-									className="slds-card__header-link slds-truncate"
-									title="First Heroku App">
-									<span>First Heroku App</span>
-								</a>
+                            <h2 className="slds-card__header-title">
+                                <a
+                                    href="javascript:void(0);"
+                                    className="slds-card__header-link slds-truncate"
+                                    title="First Heroku App">
+                                    <span>First Heroku App</span>
+                                </a>
 
-							</h2>
-						</div>
+                            </h2>
+                        </div>
                         <label className="slds-text-align_center" style={{align: "center"}}>{CurrentApp} </label>
-					</header>
-					<br />
-				</div>
-			</article>
-		);
-	}
+                    </header>
+                    <br/>
+                </div>
+            </article>
+        );
+    }
 }
