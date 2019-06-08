a177014fd3ad37b2d5767e7e0c4bb7559ff9ab8f
skarmakar5 <skarmakar@salesforce.com>
2019-06-08 18:24:36 +0200

    asd

--- client/src/postGreTableCreator.jsx
+++ client/src/postGreTableCreator.jsx
@@ -13,17 +13,21 @@ export default class postGreTableCreator extends Reflux.Component {
 		this.getalltables();
 	}
 	getalltables() {
-		
-
 		axios
 			.post("/api/getTableNames")
 			.then(response => {
 				console.log("The response ", response);
+				this.setState({
+					allPostGresTables:response.data;
+				})
 			})
 			.catch(error => {});
 	}
 	render() {
 		var listTables = [];
+		if (this.state && this.allPostGresTables!=undefined){
+			listTables
+		}
 		return (
 			<div className="slds-grid">
 				<table>
@@ -32,8 +36,12 @@ export default class postGreTableCreator extends Reflux.Component {
 							<label className="slds-text-heading_medium">Select Existing Table: </label>
 						</td>
 						<td>
-							<input />
-							<datalist />
+							<input data="postGresTables" />
+							<datalist id="postGresTables" >
+							{listTables.map(value => (
+								<option>{value}</option>
+							))}
+							</datalist>
 						</td>
 						<td>
 							<input type="button" value="Create a New Table" />
