99a90dca1516c6c017995f5783fab435b45e7887
skarmakar5 <skarmakar@salesforce.com>
2019-06-10 11:32:59 +0200

    as

--- client/src/errorAndLoading.jsx
+++ client/src/errorAndLoading.jsx
@@ -69,7 +69,7 @@ export default class ErrorAndLoading extends Reflux.Component {
 							<div className="slds-backdrop slds-backdrop_open" />
 						</div>
 					</div>
-					<Modal open={(this.state.showProgress!=undefined?this.state.showProgress:false)}>
+					<Modal open={(this.state && this.state.showProgress!=undefined?this.state.showProgress:false)}>
 						<div id="modalLoader">
 							<div
 								style={{
