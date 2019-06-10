fdc24445d30443bec16df4ad5af2df43c0a5357c
skarmakar5 <skarmakar@salesforce.com>
2019-06-10 10:18:23 +0200

    as

--- client/src/errorAndLoading.jsx
+++ client/src/errorAndLoading.jsx
@@ -4,6 +4,10 @@ import React from "react";
 import "./App.css";
 import "./lightning-design/styles/salesforce-lightning-design-system.css";
 
+var Reflux = require("reflux");
+var ContentReviewStore = require("./ContentReviewStore.jsx");
+var ContentReviewerActions = require("./ContentReviewerActions.jsx");
+
 export default class errorAndLoading extends Reflux.Component {
 	constructor(props) {
 		super();
