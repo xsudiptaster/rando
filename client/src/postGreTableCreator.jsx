a56a7f667affc8012c310ec2bb3448bf7e1f8741
skarmakar5 <skarmakar@salesforce.com>
2019-06-12 23:19:12 +0200

    asd

--- client/src/postGreTableCreator.jsx
+++ client/src/postGreTableCreator.jsx
@@ -232,14 +232,16 @@ export default class postGreTableCreator extends Reflux.Component {
 									backgroundColor: "white",
 								}}
 								className="slds-align_absolute-center">
-								<div style={{ alignItems: "center" }} >
+								<div style={{ alignItems: "center" }}>
 									<table className="slds-table">
 										<tr>
 											<td>
 												<table className="slds-table">
 													{listEvenHeader.map(value1 => (
 														<tr>
-															<td>{value1}</td>
+															<td>
+																<div className="slds-text-heading_small">{value1}</div>
+															</td>
 															<td>
 																<input type="text" />
 															</td>
@@ -249,9 +251,11 @@ export default class postGreTableCreator extends Reflux.Component {
 											</td>
 											<td>
 												<table className="slds-table">
-													{listOddHeader.map(value1 => (
+													{listOddHeader.map(value2 => (
 														<tr>
-															<td>{value1}</td>
+															<td>
+																<div className="slds-text-heading_small">{value2}</div>
+															</td>
 															<td>
 																<input type="text" />
 															</td>
